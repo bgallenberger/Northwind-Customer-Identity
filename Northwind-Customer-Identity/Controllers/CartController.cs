e112fa897731a68aacc454cef865471df9f02c1d
Brian Gallenberger <BGallenberger@my.wctc.edu>
2020-05-13 19:29:08 -0500

    stuff

---
+++ Northwind-Customer-Identity/Controllers/CartController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class CartController : Controller
+    {
+        // this controller depends on the NorthwindRepository
+        private INorthwindRepository repository;
+        public CartController(INorthwindRepository repo) => repository = repo;
+        public IActionResult Index(int id)
+        {
+            ViewBag.id = id;
+            //return View();
+            return View(repository.CartItems.OrderBy(c => c.Product.ProductName));
+        }
+    }
+}
